c2d57faf421c1b03a38dde68e6c94a97888e196c
nir.segev <nir.segev@ladbrokescoral.com>
2018-10-13 23:01:55 +0300

    additional slides added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
         super(props);
         this.state = {
             slideNum: 1,
-            numOfSlides: 14,
+            numOfSlides: 15,
             preloaderMounted: true
         };
     }
