e8de92d3872504fed97a0fd8d5d82f5f91de7ade
nir.segev <nir.segev@ladbrokescoral.com>
2018-10-13 08:25:30 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slideNum: 1,
+            numOfSlides: 10
+        };
+    }
+    setSlideNum(direction) {
+        if (direction==='prev'){
+            if(this.state.slideNum>1){
+            console.log('prev');
+                this.setState({slideNum:this.state.slideNum-1})
+            }
+        }
+        else if (direction==='next'){
+            if(this.state.slideNum<this.state.numOfSlides){
+            console.log('next');
+                this.setState({slideNum:this.state.slideNum+1})
+            }
+        };
+    }
+    presentPagination(){
+        return `Slide: ${this.state.slideNum} / ${this.state.numOfSlides}`
+    }
+  render() {
+    return (
+      <div className="App">
+        <header className="presentation-header hor-pad">
+            css-101 selectors & specificity
+        </header>
+        <main className="main hor-pad">
+
+        </main>
+        <footer className="footer">
+            <div className="pagination">
+                {this.presentPagination()}
+            </div>
+            <nav>
+                <button id="prev-slide" className="button prev" onClick={()=>{this.setSlideNum('prev')}}>prev</button>
+                <button id="next-slide" className="button next" onClick={()=>{this.setSlideNum('next')}}>next</button>
+            </nav>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
